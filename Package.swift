04edfd640813a44c868846d11a66046a8d0c24fd
Lukas Simonson <lukas@hydradesignllc.com>
2025-10-31 11:57:09 -0600

    Bumps Swift Version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax", from: "601.0.1"),
+        .package(url: "https://github.com/apple/swift-syntax", from: "602.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
